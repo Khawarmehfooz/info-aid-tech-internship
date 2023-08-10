bf39d8edd7f993c973fa93b742ed0533da9c5eb9
Khawarmehfooz <khawarmehfooz565@gmail.com>
2023-08-10 19:46:18 +0500

    Create Navbar

---
+++ Product-Landing-Page/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector('.nav__list');
+const navToggle = document.querySelector('.nav__hamburger')
+navToggle.addEventListener('click', () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true)
+    } else {
+        nav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false)
+    }
+})
